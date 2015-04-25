77a3c7906da52eab30c1d7006cd0e28f1df26c8c
Hana Um <hanau12@gmail.com>
2015-04-24 17:43:22 -0700

    Add comments

--- ProblemSet1/pset1-vowel search.py
+++ ProblemSet1/pset1-vowel search.py
@@ -1,4 +1,13 @@
-s = "haouiweu"
+'''Counting Vowels: counts up the number of vowels contained in the string 's'
+				Valid vowels are 'a','e','i','o','u'
+
+ex: 
+s = 'azcbobobegghakl' should print "Number of vowels: 5"
+
+MIT-6.001x Python
+Hana Um
+'''
+
 vowels = 0
 for letter in s:
     if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
