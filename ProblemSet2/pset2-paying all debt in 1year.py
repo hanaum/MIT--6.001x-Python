2494f214b3a056ddf1aab9abedea02bbaec2deb2
Hana Um <hanau12@gmail.com>
2015-04-24 17:57:54 -0700

    Add comments / delete unnecessary parentheses

--- ProblemSet2/pset2-paying all debt in 1year.py
+++ ProblemSet2/pset2-paying all debt in 1year.py
@@ -1,13 +1,20 @@
+'''Paying Debt Off in a Year: calculates the minimum fixed monthly payment needed in order to
+		pay off a credit card balance within 12 months. 
+
+MIT-6.001x Python
+Hana Um
+'''
+
 months = 0
 fixedPay = 0
 original_balance = balance
-monthlyInterestRate = (annualInterestRate) / 12.0
-monthlyUnpaidBalance = (balance) - (fixedPay)
-updatedBalance = (monthlyUnpaidBalance) + (monthlyInterestRate * monthlyUnpaidBalance)
+monthlyInterestRate = annualInterestRate/ 12.0
+monthlyUnpaidBalance = balance - fixedPay
+updatedBalance = monthlyUnpaidBalance + monthlyInterestRate * monthlyUnpaidBalance
 
 while balance > 0:
-    monthlyUnpaidBalance = (balance) - (fixedPay)
-    updatedBalance = (monthlyUnpaidBalance) + (monthlyInterestRate * monthlyUnpaidBalance)
+    monthlyUnpaidBalance = balance - fixedPay
+    updatedBalance = monthlyUnpaidBalance + monthlyInterestRate * monthlyUnpaidBalance
     months += 1
     balance = round(updatedBalance, 2)
 
