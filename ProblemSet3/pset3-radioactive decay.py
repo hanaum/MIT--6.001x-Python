ae2ad17584abce6006491d6f2a4d5e25a5f5059d
Hana Um <hanau12@gmail.com>
2015-04-26 20:13:05 -0700

    Add comments and test with new arguments

--- ProblemSet3/pset3-radioactive decay.py
+++ ProblemSet3/pset3-radioactive decay.py
@@ -1,3 +1,21 @@
+'''Computes and returns the amount of radiation exposed
+    to between the start and stop times. Calls the
+    function f (defined for you in the grading script)
+    to obtain the value of the function at any point.
+
+    start: integer, the time at which exposure begins
+    stop: integer, the time at which exposure ends
+    step: float, the width of each rectangle. You can assume that
+      the step size will always partition the space evenly.
+
+    returns: float, the amount of radiation exposed to
+      between start and stop times.
+
+MIT-6.001x Python
+Hana Um
+'''
+
+# pre-defined curve function for radiation
 def f(x):
     import math
     return 400*math.e**(math.log(0.5)/3.66 * x)
@@ -11,4 +29,5 @@ def radiationExposure(start, stop, step):
         time += step
     return total_exposure
 
+#test arguments
 radiationExposure(0.0, 4.0, 0.25)
